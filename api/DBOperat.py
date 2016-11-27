cb7ff909c965e1142c51f1f2be4919e7a9493e22
Aaron_Ming <aaron_ming@outlook.com>
2016-11-27 22:59:44 +0800

    添加注释

--- api/DBOperat.py
+++ api/DBOperat.py
@@ -21,19 +21,20 @@ class DBOPERAT:
             print "传入的值与表键数量不匹配应传%s,实传%s" % (keys_num, values_num)
             return False
 
-        sqlvals = copy.deepcopy(table_thead[self.table])    # 格式化sql的值
+        sqlvals = copy.deepcopy(table_thead[self.table])    # 定义格式化sql所需要的值的列表
         sqlvals.insert(0,self.table)
         
-        temp_arr = []
-        for val in values:
-            if type(val) == unicode:
-                sqlvals.append(val.encode("utf-8"))
+        temp_arr = []       # 临时list
+        for val in values:          # 遍历插入的数据,遍历完成sqlval就完整了
+            if type(val) == unicode:        # 判断元素是否为unicode字符
+                sqlvals.append(val.encode("utf-8"))         # 将unicode字符转换为utf-8类型字符串并添加到sqlval中
             else:
                 sqlvals.append(val)
         # 拼接出需要格式化的sql模型
         tmp_sql = 'insert into %s (' + ",".join(['%s'] * keys_num) + ') values (' + ",".join(['"%s"'] * values_num) + ')'
         # 格式化sql语句
         sql = tmp_sql % (tuple(sqlvals))
+        print "执行数据库操作: %s" % (sql)
         try:
             self.db.execute(sql)            # 执行添加数据的sql
         except Exception as error:
@@ -45,10 +46,9 @@ class DBOPERAT:
             return True
 
     def delete(self, id_arr):
-        print id_arr
         for _id in id_arr:
             sql = "delete from %s where id=%s" % (self.table, _id)
-            print sql
+            print "执行数据库操作: %s" % (sql)
             try:
                 self.db.execute(sql)            # 执行删除数据的sql
             except Exception as error:
@@ -61,30 +61,31 @@ class DBOPERAT:
 
     def update(self, values, _id):
         values_num = len(values)
-        if self.table == "user":
+        if self.table == "user":        # user表只更新数据不更新用户名
             keys_num = len(table_thead[self.table])-1
             tmp_thead = copy.deepcopy(table_thead[self.table])
             tmp_thead.pop(0)
-        else:
+        else:                           # 其余表字段全部可以更新
             keys_num = len(table_thead[self.table])
             tmp_thead = copy.deepcopy(table_thead[self.table])
 
-        if values_num != keys_num:                        # 将后端穿入的值与数据库表key的数量做验证,匹配即可继续
+        if values_num != keys_num:                        # 将后端传入的值与数据库表key的数量做验证,匹配即可继续
             print "传入的值%s" % (str(values))
             print "传入的值与表键数量不匹配应传%s,实传%s" % (keys_num, values_num)
             return False
-
+        # 拼接出需要格式化的sql模型
         tmp_sql = "update %s set" + ",".join([" %s='%s'"] * keys_num) + " where id='%s'"
-        sqlvals = [self.table]
-        au_val = []
+        sqlvals = [self.table]          # 定义格式化sql所需要的值的列表  
 
+        au_val = []         # 将sql的key和对应的value作为一个元祖插入到此列表作为格式化的值用
         for i in range(keys_num):
             au_val.append((tmp_thead[i],values[i].encode("utf-8")))
+        # 遍历au_val,添加到sqlval里
         for x,y in au_val:
             sqlvals.append(x)
             sqlvals.append(y)
         sqlvals.append(_id)
-
+        # 格式化sql语句
         sql = tmp_sql % (tuple(sqlvals))
         print "执行数据库操作: %s" % (sql)
         try:
@@ -105,7 +106,7 @@ class DBOPERAT:
         tmp_sql = "select id," + ",".join(['%s'] * num) + " from %s"       # 格式化出查询的sql
 
         sql = tmp_sql % tuple(values)
-
+        print "执行数据库操作: %s" % (sql)
         try:
             res = self.db.execute(sql)            # 执行查询数据的sql
         except Exception as error:
